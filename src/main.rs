ee400b6f41342ed8a6988182300a6c7c8ff52585
itsara <khajer@gmail.com>
2023-11-16 15:22:54 +0000

    add actix & create firstversion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Version {
+    version: String,
+}
+
+#[get("/version")]
+async fn version() -> Result<impl Responder> {
+    let version = Version {
+        version: "1.0.0".to_string(),
+    };
+    Ok(web::Json(version))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{App, HttpServer};
+
+    HttpServer::new(|| App::new().service(version))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
