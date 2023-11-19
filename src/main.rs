a9390ccacfd09302a7bf56c14e4e16107cc31052
itsara <khajer@gmail.com>
2023-11-19 16:13:57 +0000

    add websocket in /ws path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, web, Responder, Result};
+use actix::{Actor, StreamHandler};
+use actix_web::{get, web, Error, HttpRequest, HttpResponse, Responder, Result};
+use actix_web_actors::ws;
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -14,12 +16,36 @@ async fn version() -> Result<impl Responder> {
     Ok(web::Json(version))
 }
 
+struct MyWs;
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
 
-    HttpServer::new(|| App::new().service(version))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(version)
+            .route("/ws", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
