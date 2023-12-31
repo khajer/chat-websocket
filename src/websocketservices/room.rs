c4cee1d7e3bd2084c1b5bf180c72b85de675c732
itsara <khajer@gmail.com>
2023-12-31 21:42:37 +0700

    add room status

--- src/websocketservices/room.rs
+++ src/websocketservices/room.rs
@@ -1,6 +1,13 @@
 use std::collections::HashSet;
 
+pub enum RoomStatus {
+    WAIT,
+    START,
+    END,
+}
+
 pub struct Room {
     members: HashSet<usize>,
     owner: usize,
+    status: RoomStatus,
 }
